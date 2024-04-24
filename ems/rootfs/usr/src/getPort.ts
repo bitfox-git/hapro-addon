65adfdcd81451200f14612c413f27a7dafb6a498
Daniël <daniel@dutchgrit.nl>
2024-04-24 07:12:48 +0000

    update getPort.ts

--- ems/rootfs/usr/src/getPort.ts
+++ ems/rootfs/usr/src/getPort.ts
@@ -3,6 +3,6 @@ const data = await fetch(`${baseUrl}/core/info`, {
   headers,
 }).then((r) => r.json());
 
-// if this request fails im sure itll be noticed elsewere lol
+// if this request fails im sure it'll be noticed somewhere else lol
 
 Deno.stdout.write(new TextEncoder().encode(`${data.data.port}`));
